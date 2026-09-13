b4a2310f9f7c821941c59f4d41a1b50a5624f8df
Steve Forse <steve.forse@gmail.com>
2026-09-13 11:51:22 -0700

    Configure MusicXML Zeitwerk inflections

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Playtab
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoloaders.main.inflector.inflect(
+      "musicxml_builder" => "MusicXmlBuilder",
+      "full_musicxml_builder" => "FullMusicxmlBuilder"
+    )
 
     # Configuration for the application, engines, and railties goes here.
     #
