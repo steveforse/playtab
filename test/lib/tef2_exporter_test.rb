cafbc1940abf301ad2fbb12b356f50183e0aca34
Steve Forse <steve.forse@gmail.com>
2026-09-14 15:07:42 -0700

    Fix TEF2 lyric block encoding

--- test/lib/tef2_exporter_test.rb
+++ test/lib/tef2_exporter_test.rb
@@ -25,7 +25,8 @@ class Tef2ExporterTest < ActiveSupport::TestCase
     end
     tail_offset += parsed[:chords].length * 32
     lyrics_length = result[:bytes].byteslice(tail_offset, 2).unpack1("v")
-    assert_equal 0, result[:bytes].getbyte(tail_offset + 2)
+    assert_equal "LYRICS &", result[:bytes].byteslice(tail_offset + 2, 9)
+    assert_equal 0, result[:bytes].getbyte(tail_offset + 2 + lyrics_length - 1)
     tail_offset += lyrics_length + 2
     assert_equal 5, result[:bytes].getbyte(tail_offset)
     assert_equal 99, result[:bytes].getbyte(tail_offset + 4)
